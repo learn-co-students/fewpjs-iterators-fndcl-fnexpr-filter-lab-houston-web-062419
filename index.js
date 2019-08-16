a5e92411badd6889865bc6fe48603df9a548fc97
abideen11 <abideenkareem17@gmail.com>
2019-08-16 14:16:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+//i === d.substring(0,2)
+function findMatching(drivers,anyDriver) {
+    const matchDrivers = drivers.filter(i => {return i.toUpperCase() === anyDriver.toUpperCase()})
+    return matchDrivers
+}
+function fuzzyMatch(drivers,d) {
+    const matchDrivers = drivers.filter(i => {return i.startsWith(d)})
+    return matchDrivers
+}
+function matchName(drivers,denote) {
+    const matchDrivers = drivers.filter(i => {return i.name === denote})
+    return matchDrivers
+}
+
